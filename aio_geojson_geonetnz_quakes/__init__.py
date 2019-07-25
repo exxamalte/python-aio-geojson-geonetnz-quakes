45ddbfa7c511ef1e1ab43271aaf3f3a400f9208a
Malte Franken <exxamalte@users.noreply.github.com>
2019-07-25 18:43:50 +1000

    expose main classes directly

--- aio_geojson_geonetnz_quakes/__init__.py
+++ aio_geojson_geonetnz_quakes/__init__.py
@@ -1,3 +1,3 @@
-"""
-aio_geojson_geonetnz_quakes library.
-"""
+"""aio_geojson_geonetnz_quakes library."""
+from .feed import GeonetnzQuakesFeed  # noqa
+from .feed_manager import GeonetnzQuakesFeedManager  # noqa
