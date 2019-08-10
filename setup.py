2274230eb6c8ccc0f04763f6cec8f4658a028a37
Malte Franken <exxamalte@users.noreply.github.com>
2019-08-10 14:15:57 +1000

    Update upstream aio_geojson_client library (#1)
    * fixed status codes in doco
    
    * bump version of upstream library
    
    * added new ability to register status update callback
    
    * fixed tests due to change in precision of long/lat in geojson library

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = "An async GeoJSON client library for GeoNet NZ Quakes feed."
 URL = "https://github.com/exxamalte/python-aio-geojson-geonetnz-quakes"
 
 REQUIRES = [
-    'aio_geojson_client>=0.3',
+    'aio_geojson_client>=0.4',
     'aiohttp>=3.5.4',
     'pytz>=2019.01',
 ]
