37e45c32b905f45047b25dbb37c0873dd41941dc
Malte Franken <exxamalte@users.noreply.github.com>
2020-02-17 21:30:56 +1100

    update upstream library

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = "An async GeoJSON client library for GeoNet NZ Quakes feed."
 URL = "https://github.com/exxamalte/python-aio-geojson-geonetnz-quakes"
 
 REQUIRES = [
-    'aio_geojson_client>=0.11',
+    'aio_geojson_client>=0.12',
     'aiohttp>=3.5.4',
     'pytz>=2019.01',
 ]
