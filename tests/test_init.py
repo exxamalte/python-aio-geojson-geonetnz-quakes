eb1ce60330cc4fb2fb73a844ab8cbd8ae411f5e6
Malte Franken <exxamalte@users.noreply.github.com>
2023-01-24 08:36:31 +1100

    Improve test coverage (#26)

---
+++ tests/test_init.py
@@ -0,0 +1,7 @@
+"""Test for the GeoNet NZ Quakes GeoJSON general setup."""
+from aio_geojson_geonetnz_quakes import __version__
+
+
+def test_version():
+    """Test for version tag."""
+    assert __version__ is not None
