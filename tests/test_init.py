69eec912feb0395770ecb8afd2318c687bd0dc81
Malte Franken <exxamalte@users.noreply.github.com>
2024-10-07 02:04:31 +1100

    Migrate to pyproject.toml (#42)
    * migrate to pyproject.toml
    
    * remove tests from source dist

--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-"""Test for the GeoNet NZ Quakes GeoJSON general setup."""
-
-from aio_geojson_geonetnz_quakes import __version__
-
-
-def test_version():
-    """Test for version tag."""
-    assert __version__ is not None
